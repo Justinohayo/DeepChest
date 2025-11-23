85e0913d1d6ed4fa67147b04304e5807427d2b71
Justinohayo <159392063+Justinohayo@users.noreply.github.com>
2025-11-23 15:45:38 -0800

    Update SQL credential for web deployment in app.py

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ app.secret_key = '1234'  # Needed for flash messages and sessions
 
 # Database connection config
 db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'test1234',
-    'database': 'DeepChest'
+    'host': 'mysql-database.cjogeuu2gnn5.ca-central-1.rds.amazonaws.com',
+    'user': 'admin',
+    'password': 'AdminIPA',
+    'database': 'DeepChest_DB'
 }
 
 # Configures Route for the home page
